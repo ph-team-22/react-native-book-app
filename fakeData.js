db915f2b928a994b1969e78a7160b5cc67379205
Prodip M <munshiprodip@gmail.com>
2021-06-03 01:54:50 +0600

    Add fakeData

---
+++ fakeData.js
@@ -0,0 +1,294 @@
+const fakeData = [
+  {
+    id: 1,
+    title: "Clean Code",
+    author: "Robert Cecil Martin",
+    imageLink:
+      "https://books.google.com.bd/books/publisher/content?id=_i6bDeoCQzsC&printsec=frontcover&img=1&zoom=1&h=640&stbn=1",
+
+    link: "https://books.google.com.bd/books?id=hjEFCAAAQBAJ&source=gbs_book_other_versions",
+    pages: 431,
+    description:
+      "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way. Noted software expert Robert C. Martin presents a revolutionary paradigm with Clean Code: A Handbook of Agile Software Craftsmanship . Martin has teamed up with his colleagues from Object Mentor to distill their best agile practice of cleaning code “on the fly” into a book that will instill within you the values of a software craftsman and make you a better programmer–but only if you work at it. What kind of work will you be doing? You’ll be reading code–lots of code. And you will be challenged to think about what’s right about that code, and what’s wrong with it. More importantly, you will be challenged to reassess your professional values and your commitment to your craft. Clean Code is divided into three parts. The first describes the principles, patterns, and practices of writing clean code. The second part consists of several case studies of increasing complexity. Each case study is an exercise in cleaning up code–of transforming a code base that has some problems into one that is sound and efficient. The third part is the payoff: a single chapter containing a list of heuristics and “smells” gathered while creating the case studies. The result is a knowledge base that describes the way we think when we write, read, and clean code.",
+    reviews: [
+      {
+        name: "brikis98",
+        rating: 5,
+        review:
+          "A good book to read for any coder - perhaps not as thorough as Code Complete but much more effective than Pragmatic Programmer. This book's biggest strength is that it includes tons of code examples ...",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "Essential book if you were-are-want to be- a programmer.",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    title: "The pragmatic programmer",
+    author: "Andrew Hunt, David Thomas",
+    imageLink:
+      "https://books.google.com.bd/books/content?id=5wBQEp6ruIAC&printsec=frontcover&img=1&zoom=5&h=640",
+
+    link: "https://www.worldcat.org/title/pragmatic-programmer-from-journeyman-to-master/oclc/1019732973",
+    pages: 320,
+    description:
+      "What others in the trenches say about The Pragmatic Programmer ... & ldquo;The cool thing about this book is that it & rsquo;s great for keeping the programming process fresh. The book helps you to continue to grow and clearly comes from people who have been there. & rdquo; & mdash; Kent Beck, author of Extreme Programming Explained: Embrace Change & ldquo;I found this book to be a great mix of solid advice and wonderful analogies! & rdquo; & mdash; Martin Fowler, author of Refactoring and UML Distilled & ldquo;I would buy a copy, read it twice, then tell all my colleagues to run out and grab a copy.",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review:
+          "Essential for the aspiring programmer. Some sections are still an unattainable ideal to me, but they all make sense and I am getting closer to that ideal day by day, inch by inch.",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review:
+          "So many programming books try and force you into a mold created through years of trial and error centered around a single static part of the industry. In the Pragmatic Programmer, Hunt and Thomas ignore the preconceptions on focus on modular methods of thinking that when followed closely teach not only better programming techniques but better LIVING techniques. And when you live better, you program better. It's that simple. ",
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    title: "Code Complete",
+    author: " Steve McConnell",
+    imageLink:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1396837641l/4845.jpg",
+
+    link: "https://www.goodreads.com/book/show/4845.Code_Complete",
+    pages: 914,
+    description:
+      "Widely considered one of the best practical guides to programming, Steve McConnell's original CODE COMPLETE has been helping developers write better software for more than a decade. Now this classic book has been fully updated and revised with leading-edge practices--and hundreds of new code samples--illustrating the art and science of software construction. Capturing the ..",
+    reviews: [
+      {
+        name: "Erika RS",
+        rating: 5,
+        review:
+          "Code Complete is a massive work, so this summary is, necessarily, very high level. It is not a book that one can absorb completely in one reading, but one can absorb its high level themes (summarized nicely in the second to last chapter).",
+      },
+      {
+        name: "Jon Fuller",
+        rating: 5,
+        review:
+          "I came into this with super high expectations. Things I'd heard people say: 'I make everyone on my team read this.' or 'Every developer should start with this book'. So, I thought, 'sweet, a great 'back to basics' book... I can't wait!'. I read through this book with a host of colleagues all with quite different experience levels and in different areas of expertise.",
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    title: "Refactoring: Improving the Design of Existing Code",
+    author: "Martin Fowler, Kent Beck, John Brant, William Opdyke, Don Roberts",
+    imageLink:
+      "https://books.google.com.bd/books/content?id=HmrDHwgkbPsC&printsec=frontcover&img=1&zoom=1&h=640",
+
+    link: "https://books.google.com.bd/books?id=HmrDHwgkbPsC&source=gbs_navlinks_s",
+    pages: 99998,
+    description:
+      "As the application of object technology--particularly the Java programming language--has become commonplace, a new problem has emerged to confront the software development community. Significant numbers of poorly designed programs have been created by less-experienced developers, resulting in applications that are inefficient and hard to maintain and extend. Increasingly, software system professionals are discovering just how difficult it is to work with these inherited, 'non-optimal' applications. For several years, expert-level object programmers have employed a growing collection of techniques to improve the structural integrity and performance of such existing",
+    reviews: [
+      {
+        name: "scottjpearson - LibraryThing",
+        rating: 5,
+        review:
+          "I picked up this book at the wrong time. The book was so successful that a second edition is due out on November 30, 2018 (less than two weeks from now). On the other hand, I picked up this book at ...",
+      },
+      {
+        name: "pgSundling - LibraryThing",
+        rating: 5,
+        review:
+          "We're spoiled now that a modern IDE like Eclipse has so many build in refactoring tools, but this was a pivotal book in programming.",
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+
+  {
+    id: 6,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+
+  {
+    id: 7,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+
+  {
+    id: 8,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+
+  {
+    id: 9,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+
+  {
+    id: 10,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+
+  {
+    id: 11,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+
+  {
+    id: 12,
+    title: "",
+    author: "",
+    imageLink: "",
+
+    link: "",
+    pages: 0,
+    description: "",
+    reviews: [
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+      {
+        name: "User review",
+        rating: 5,
+        review: "",
+      },
+    ],
+  },
+];
+
+export default fakeData;
