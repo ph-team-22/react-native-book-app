cba1b011c631ca6955387d36a5eac46be163d839
Musiur Alam Opu <74836476+MusiurAlamOpu@users.noreply.github.com>
2021-06-04 18:01:00 +0600

    after completing the details page

--- components/BookDetails/BookDetails.js
+++ components/BookDetails/BookDetails.js
@@ -1,42 +1,179 @@
-import React from "react";
-import { Text, View, Image, Button } from "react-native";
-import Books from "../../Images/Books.jpg";
+import React, { useEffect, useState } from "react";
+import { Text, View, Image, Button, StyleSheet } from "react-native";
+import BookImage from "../../Images/Books.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faStar } from "@fortawesome/free-solid-svg-icons";
+import axios from "react-native-axios";
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+    // height: "100%",
+    alignItems: "center",
+  },
+});
 
 const BookDetails = () => {
+  const bookDetails = {
+    title: "Schaum's outlines Programming With C",
+    author: "Byron Gottfried",
+    edition: "Third",
+    description:
+      "Extensive coverage of C Programming concepts || new chapter on Strings || Included new topics - Fundamentals of Computers; Introduction to Algorithms and Pseducodes; Random and Sequential Access of Files|| Over 1200 problems, review questions, programming exercises and frequesntly asked questions.",
+    imageLink:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1533393342l/41042739._SX318_.jpg",
+    link: "https://www.goodreads.com/book/show/41042739-programming-with-c-sie",
+    reviews: [
+      {
+        name: "Musiur",
+        rating: 5,
+        comment: "I am satisfied.",
+      },
+      {
+        name: "Alam",
+        rating: 5,
+        comment: "I Have enjoyed the book.",
+      },
+      {
+        name: "Opu",
+        rating: 5,
+        comment: "Thanks for your ontime service",
+      },
+    ],
+    category: "Programming",
+    overallRating: 5,
+  };
+  const { title, author, edition, description, imageLink, link, reviews } =
+    bookDetails;
+  console.log(title, author, edition, description, imageLink, link, reviews);
+  console.log(reviews[0].name);
+
   const handlePress = () => {};
   return (
-    <View
-      style={{
-        paddingTop: 50,
-        textAlign: "center",
-        backgroundColor: "#F2F2F2",
-      }}
-    >
-      <FontAwesomeIcon
-        style={{ padding: 12, margin: 10 }}
-        color="grey"
-        icon={faArrowLeft}
-      />
-      <Image
-        source={Books}
+    <View style={styles.container}>
+      <View
         style={{
-          width: 400,
-          height: 300,
-          paddingTop: 2,
-        }}
-      />
-      <Text
-        style={{
-          textAlign: "center",
-          fontSize: 20,
-          marginTop: 5,
-          fontWeight: "bold",
+          paddingTop: 50,
+          alignSelf: "center",
         }}
       >
-        Bundle of Three Category
-      </Text>
+        <FontAwesomeIcon
+          style={{ padding: 12, margin: 10 }}
+          color="grey"
+          icon={faArrowLeft}
+        />
+        <Image
+          source={BookImage}
+          style={{
+            width: 400,
+            height: 500,
+            paddingTop: 2,
+            alignSelf: "center",
+          }}
+        />
+        <Text
+          style={{
+            alignSelf: "center",
+            fontSize: 12,
+            marginTop: 5,
+            fontWeight: "bold",
+            paddingLeft: 10,
+            color: "#F07427",
+          }}
+        >
+          {/* Bundle of Three Category */}
+          {bookDetails.category}
+        </Text>
+        <Text
+          style={{
+            alignSelf: "center",
+            fontSize: 20,
+            marginTop: 5,
+            fontWeight: "bold",
+            paddingLeft: 10,
+          }}
+        >
+          {bookDetails.title}
+        </Text>
+        <Text
+          style={{
+            alignSelf: "center",
+            fontSize: 12,
+            marginTop: 5,
+            fontWeight: "bold",
+            paddingLeft: 10,
+          }}
+        >
+          by{" "}
+          <Text
+            style={{
+              alignSelf: "center",
+              fontSize: 14,
+              marginTop: 5,
+              fontWeight: "bold",
+              paddingLeft: 10,
+              color: "green",
+            }}
+          >
+            {bookDetails.author}
+          </Text>
+          <Text
+            style={{
+              alignSelf: "center",
+              fontSize: 18,
+              marginTop: 5,
+              fontWeight: "bold",
+              paddingLeft: 10,
+            }}
+          >
+            {" | "}
+          </Text>
+          <Text
+            style={{
+              alignSelf: "center",
+              alignItems: "center",
+              fontSize: 12,
+              marginTop: 5,
+              fontWeight: "bold",
+              paddingLeft: 10,
+            }}
+          >
+            <FontAwesomeIcon color="#F07427" icon={faStar} />
+            <FontAwesomeIcon color="#F07427" icon={faStar} />
+            <FontAwesomeIcon color="#F07427" icon={faStar} />
+            <FontAwesomeIcon color="#F07427" icon={faStar} />
+            <FontAwesomeIcon color="#F07427" icon={faStar} />
+            {/* {bookDetails.overallRating} */}
+            <Text
+              style={{
+                alignSelf: "center",
+                fontSize: 14,
+                marginTop: 5,
+                fontWeight: "bold",
+                paddingLeft: 10,
+              }}
+            >
+              {"  "}
+              {bookDetails.overallRating} {" stars"}
+            </Text>
+          </Text>
+        </Text>
+      </View>
+      <View style={{ marginTop: 10 }}>
+        <Button
+          title="Read Now!"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </View>
+      <View style={{ padding: 10 }}>
+        <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+          Book's Description
+        </Text>
+        <Text>{bookDetails.description}</Text>
+      </View>
     </View>
   );
 };
